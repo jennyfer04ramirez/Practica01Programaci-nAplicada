02c1600799446f3bbed392fa4fb3a88b5ce41206
jennyfer04ramirez <jr.6311.camm@gmail.com>
2023-11-06 00:37:30 -0500

    Creación de las clases :)

---
+++ src/main/java/ec/edu/ups/ramirezc_quitokpracticauno/Inicio.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ups.ramirezc_quitokpracticauno;
+
+/**
+ *
+ * @author casa
+ */
+public class Inicio {
+
+    public static void main(String[] args) {
+        // Se crea las instancias de las clases 
+        DataHolder datH = new DataHolder();
+        DataPrinter datP = new DataPrinter();
+        DataProcessor datPP = new DataProcessor();
+    }
+}
